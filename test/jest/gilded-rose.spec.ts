53c1d7dbe18ed4e6f9257dd16730dd1d5b9f1d65
javiducun <javiducun@gmail.com>
2023-07-22 09:51:04 +0200

    test: sulfuras allways 80

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,15 @@
 import { Item, GildedRose } from "@/gilded-rose";
 
 describe("Gilded Rose", () => {
-  it("should foo", () => {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+  it("Sulfuras is allways 80", () => {
+    const gildedRose = new GildedRose([
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+    ]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(80);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(80);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(80);
   });
 });
