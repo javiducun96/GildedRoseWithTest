a350de715f1d7e01d97ffef24b09dafa2f16747d
javiducun <javiducun@gmail.com>
2023-07-22 10:09:29 +0200

    quality is allways positive

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -24,4 +24,22 @@ describe("Gilded Rose", () => {
     items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(44);
   });
+
+  it("article quality is never negative", () => {
+    const initialItems = [
+      new Item("Normal article", 2, 50),
+      new Item("Normal article", 2, 0),
+      new Item("Aged Brie", 2, 3),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 2, 1),
+      new Item("Sulfuras, Hand of Ragnaros", 2, 80),
+    ];
+    const gildedRose = new GildedRose(initialItems);
+    for (let i = 0; i < 50; i++) {
+      gildedRose.updateQuality();
+    }
+    let items = gildedRose.updateQuality();
+    items.forEach((item) => {
+      expect(item.quality).toBeGreaterThanOrEqual(0);
+    });
+  });
 });
